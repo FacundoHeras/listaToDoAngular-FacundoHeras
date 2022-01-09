91a1a1c4aa1de4d1c4601dd22eb64aee4aa56135
FacundoHeras <40032612+FacundoHeras@users.noreply.github.com>
2022-01-09 18:17:58 -0300

    cambios finales

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/proyecto-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/proyecto-angular/'}),
+    res.sendFile('index.html', {root: 'dist/proyecto-angular/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
