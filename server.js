7b17132694e881a3090cbee7c1f096d5fa882080
FacundoHeras <40032612+FacundoHeras@users.noreply.github.com>
2022-01-09 19:02:07 -0300

    cambios finales 4

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/proyectos-angular'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/proyecto-angular/index.html'));});
+'/dist/proyectos-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
